a6fe1421319d131aa370a5cb916ccf226bbf3ab3
Alexander Kehr <alex@alexanderkehr.de>
2023-05-10 16:43:58 +0200

    fix load image from url cors error

--- client/src/load-image.svelte
+++ client/src/load-image.svelte
@@ -2,37 +2,38 @@
   let url = '';
   export let onImageLoaded: (img: HTMLImageElement) => void;
 
-  const loadImage = () => {
-    if (url) {
-      const img = new Image();
-      img.src = url;
-      img.onload = () => {
-        onImageLoaded(img);
-      };
-    }
-  };
+  function loadImage(src: string, crossOrigin = false) {
+    const img = new Image();
+    img.crossOrigin = crossOrigin ? 'anonymous' : '';
+    img.src = src;
+    img.onload = () => {
+      onImageLoaded(img);
+    };
+  }
 
-  const chooseFile = (e: Event) => {
+  function onLoadURL() {
+    if (url) {
+      loadImage(url, true);
+    }
+  }
+
+  function chooseFile(e: Event) {
     const input = e.target as HTMLInputElement;
     if (input.files && input.files[0]) {
       const file = input.files[0];
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
-        const img = new Image();
-        img.src = reader.result as string;
-        img.onload = () => {
-          onImageLoaded(img);
-        };
+        loadImage(reader.result as string);
       };
     }
-  };
+  }
 </script>
 
 <div>
   <label for="url">Image URL:</label>
   <input type="text" id="url" bind:value={url} />
-  <button on:click={loadImage}>Load Image</button>
+  <button on:click={onLoadURL}>Load Image</button>
 </div>
 
 <div>
