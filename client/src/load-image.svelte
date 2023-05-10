0e9c17cdc030e221b23ddcb3ba9f68344ad187da
Alexander Kehr <alex@alexanderkehr.de>
2023-05-10 16:02:54 +0200

    implement load image immediatly after choosing

--- client/src/load-image.svelte
+++ client/src/load-image.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   let url = '';
-  let file = null;
   export let onImageLoaded: (img: HTMLImageElement) => void;
 
   const loadImage = () => {
@@ -10,7 +9,13 @@
       img.onload = () => {
         onImageLoaded(img);
       };
-    } else if (file) {
+    }
+  };
+
+  const chooseFile = (e: Event) => {
+    const input = e.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
@@ -22,23 +27,15 @@
       };
     }
   };
-
-  const chooseFile = (e: Event) => {
-    const input = e.target as HTMLInputElement;
-    if (input.files && input.files[0]) {
-      file = input.files[0];
-    }
-  };
 </script>
 
 <div>
   <label for="url">Image URL:</label>
   <input type="text" id="url" bind:value={url} />
+  <button on:click={loadImage}>Load Image</button>
 </div>
 
 <div>
   <label for="file">Image file:</label>
   <input type="file" id="file" on:change={chooseFile} accept="image/*" />
 </div>
-
-<button on:click={loadImage}>Load Image</button>
